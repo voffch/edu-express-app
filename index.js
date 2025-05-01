72873ae82487ef9de1994db26c827df86b86e97d
voffch <voffch@gmail.com>
2025-05-01 19:35:39 +0500

    change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello <a href="https://courses.mooc.fi/org/uh-cs/courses/devops-with-docker/chapter-4/deployment-pipelines">Deployment pipelines</a>!</h1>')
 })
 
 const PORT = 8080
